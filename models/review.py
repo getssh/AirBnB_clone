00802045c8e387c09f68802b4b37d382834f0f5b
root <root@DESKTOP-F2BLBCH.localdomain>
2022-02-16 08:29:10 +0300

    review updated

--- models/review.py
+++ models/review.py
@@ -1,10 +1,19 @@
-#!/uxr/bin/python3
-"""this is the review module"""
-from models.base_model import BaseModel
+#!/usr/bin/python3
+""" Review module for the HBNB project """
+from sqlalchemy.sql.schema import ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
 
 
-class Review(BaseModel):
-    """this class is for review"""
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    if storage_type == 'db':
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
